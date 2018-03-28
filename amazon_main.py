2dafc481aa459cf922a71a08b084245d47664354
oleh hrebchuk <oleh.hrebchuk@probegin.com>
2018-03-28 21:30:18 +0300

    add main file

---
+++ amazon_main.py
@@ -0,0 +1,17 @@
+import urllib.request
+from time import sleep
+
+
+def url(year, day):
+    return 'https://s3.amazonaws.com/detailed-billing-test/615271354814-aws-billing-detailed-line-items-with-resources-and-tags-{}-{}.csv.zip'.format(year, day)
+
+
+year = '2016'
+dates = ['05', '06', '07', '08']
+download_folder = 'download/'
+
+for day in dates:
+    print(url(year, day))
+    urllib.request.urlretrieve(url(year, day), '{}{}-{}.zip'.format(download_folder, year, day))
+    sleep(5)
+
