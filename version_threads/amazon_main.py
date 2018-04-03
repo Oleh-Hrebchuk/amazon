03cfe99467d64a22842744d30509e6cec32d24c7
Yura <hulpa.yura@gmail.com>
2018-04-04 00:22:15 +0300

    added index added write to DB from threads

--- version_threads/amazon_main.py
+++ version_threads/amazon_main.py
@@ -9,22 +9,26 @@ from collections import defaultdict
 
 from download_files import DownloadFiles
 from unzip_files import UnzipFiles
-import sqlite_manager
+from sqlite_manager import create_table
 
 
 year = '2016'
 dates = ['05', '06', '07', '08']
 download_folder = 'downloads/'
-unzipped_files = 'unzipped_files'
+unzipped_files = 'unzipped_files/'
 sclar = defaultdict(list)
 pattern = re.compile(r'v1:\d+:\d+:\d+:\w+-\w+-\w+-\w+-\w+\Z')
-csv_files = [unzipped_files + '/' + file for file in os.listdir(unzipped_files)]
 
-conn = sqlite3.connect('amazon.sqlite3')
+
+# check_same_thread - to allow threading with SQlite
+conn = sqlite3.connect('amazon.sqlite3', check_same_thread=False)
 cursor = conn.cursor()
 
 
-def write_cost_data(data, slice='v1:'):
+lock = threading.Lock()
+
+
+def write_cost_data(data, _slice='v1:'):
     for key, val in data.items():
         db_val = cursor.execute(
             "SELECT * FROM cost WHERE object_type = '{}'".format(
@@ -40,26 +44,33 @@ def write_cost_data(data, slice='v1:'):
                 "UPDATE cost SET cost={} WHERE object_type = '{}'".format(suma, key))
             conn.commit()
         else:
-
             suma = sum(val)
             cursor.execute(
                 "INSERT INTO cost VALUES ('{}', '{}', {})".format(
-                    key, key[len(slice):], suma))
+                    key, key[len(_slice):], suma))
             conn.commit()
 
 
 def cost(file):
+    """
+    Calculate costs and write them into SQlite with lock context manager
+    """
+    row_results = {}
     with open(file) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             user = row['user:scalr-meta']
             cost = row['Cost']
             if pattern.match(user):
-                if user not in sclar:
-                    sclar[user] = [float(cost)]
+                if user not in row_results:
+                    row_results[user] = [float(cost)]
                 else:
-                    sclar[user].append(float(cost))
+                    row_results[user].append(float(cost))
 
+    # according to python docs, there are issues when writing from threads into SQlite. So we need to set lock.
+    # https://docs.python.org/3/library/sqlite3.html#multithreading
+    with lock:
+        write_cost_data(data=row_results)
 
 # set env
 if not os.path.exists(download_folder):
@@ -68,45 +79,65 @@ if not os.path.exists(download_folder):
 if not os.path.exists(unzipped_files):
     os.mkdir(unzipped_files)
 
-# download
-queue = Queue()
-for i in range(len(dates)):
-    d = DownloadFiles(queue, year, download_folder)
-    d.setDaemon(True)
-    d.start()
 
-for url in dates:
-    queue.put(url)
+def prepare():
+    """
+    Download and unzip files.  
+    """
 
-queue.join()
+    # download
+    queue = Queue()
+    for i in range(len(dates)):
+        d = DownloadFiles(queue, year, download_folder)
+        d.setDaemon(True)
+        d.start()
 
-# unzip
-queue_unzip = Queue()
-for i in range(len(dates)):
-    d = UnzipFiles(queue_unzip, year, download_folder, unzipped_files)
-    d.setDaemon(True)
-    d.start()
+    for url in dates:
+        queue.put(url)
 
-for url in dates:
-    queue_unzip.put(url)
+    queue.join()
 
-queue_unzip.join()
+    # unzip
+    queue_unzip = Queue()
+    for i in range(len(dates)):
+        d = UnzipFiles(queue_unzip, year, download_folder, unzipped_files)
+        d.setDaemon(True)
+        d.start()
 
-# calc
-threads = []
-for file in csv_files:
-    threads.append(threading.Thread(target=cost, args=(file,)))
-for t in threads:
-    t.start()
-for t in threads:
-    t.join()
+    for url in dates:
+        queue_unzip.put(url)
 
-# write data
-import time
-start_time = time.time()
-write_cost_data(sclar)
-cursor.close()
-print("--- %s seconds ---" % (time.time() - start_time))
+    queue_unzip.join()
 
 
+def calculate_in_threads(csv_files_list):
+    """
+    Spawn thread per csv file and join them
+    :param csv_files_list:  
+    """
+    threads = []
+    for file in csv_files_list:
+        threads.append(threading.Thread(target=cost, args=(file,)))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
 
+
+if __name__ == "__main__":
+    import time
+
+    # call prepare if you want download and unzip files
+    # uncomment oif you want to download and unzip
+
+    # prepare()
+
+    create_table()
+
+    csv_files = [unzipped_files + '/' + file for file in os.listdir(unzipped_files)]
+
+    start_time = time.time()
+
+    calculate_in_threads(csv_files_list=csv_files)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
--- version_threads/sqlite_manager.py
+++ version_threads/sqlite_manager.py
@@ -1,8 +1,7 @@
 import sqlite3
 
 
-if __name__ == "__main__":
-    # Create database and table.
+def create_table():
     conn = sqlite3.connect('amazon.sqlite3')
     cursor = conn.cursor()
 
@@ -10,8 +9,19 @@ if __name__ == "__main__":
     CREATE TABLE IF NOT EXISTS cost (
     object_type text NOT NULL,
     object_id VARCHAR(32) NOT NULL,
-    cost float NOT NULL)
+    cost float NOT NULL);
+    """
+
+    # set index as object_type is criteria of record uniqueness
+    create_index = """
+    CREATE INDEX IF NOT EXISTS cost_object_type_idx ON cost (object_type);
     """
 
     cursor.execute(create_cost)
+    cursor.execute(create_index)
     cursor.close()
+
+if __name__ == "__main__":
+    # Create database and table.
+    create_table()
+
